31765d1913a1453583eb857b32f513420a493fc9
Abe Reyes <abe.raise@gmail.com>
2024-01-07 16:50:24 -0500

    Refactor user list component to use transition group

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,14 +1,16 @@
 <template>
-
-  <ul>
-    <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-  </ul>
+  
+    <transition-group tag="ul" name="user-list">
+      <li v-for="user in users" :key="user" @click="removeUser(user)">
+        {{ user }}
+      </li>
+    </transition-group>
+  
 
   <div>
     <input type="text" ref="userNameInput" />
     <button @click="addUser">Add User</button>
   </div>
-
 </template>
 
 <script>
@@ -24,9 +26,9 @@ export default {
       this.users.unshift(enteredUserName);
     },
     removeUser(user) {
-      this.users=this.users.filter(usr => usr !== user)
+      this.users = this.users.filter((usr) => usr !== user);
     },
-  }
+  },
 };
 </script>
 
@@ -42,4 +44,30 @@ li {
   padding: 1rem;
   text-align: center;
 }
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px)
+}
+
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+
+.user-list-leave-active {
+  transition: all 1s ease-in;
+}
+
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
 </style>
